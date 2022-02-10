559e585e4b0886539c487c160b16e5fac2f85efc
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2022-02-10 11:29:38 +0100

    Added best and worst Quotes methods to app comp instead ngIf directive at ranking html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,18 +10,30 @@ import { Quotation } from './models/quotation';
 export class AppComponent {
   showForm = false;
   quotes: Quotation[] = QUOTES;
+  //pole quotation reprezentuje pojedynczy cytat
   quotation: Quotation = { author: '', sentence: '', votes: 0 };
 
+  // przełącza pole klasy true / false
   onSwitchForm(): void {
     this.showForm = !this.showForm;
   }
 
+    // dodaje cytat na początek listy i resetuje pole quotation
   addQuotation() {
     this.quotes.unshift(this.quotation);
     this.quotation = { author: '', sentence: '', votes: 0 };
   }
 
+  // metoda obsługuje głosowanie na konkretny cytat
   addVote(quotation: Quotation, value: number) {
     quotation.votes += value;
+  }
+
+  bestQuotes() {
+    return this.quotes.filter(q => q.votes > 0);
+  }
+
+  worstQuotes() {
+    return this.quotes.filter(q => q.votes < 0);
   }
 }
